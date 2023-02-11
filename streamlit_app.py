4edcff65a35ba9233b7382247e89535fa585d79e
AbhilashB36 <124914107+AbhilashB36@users.noreply.github.com>
2023-02-11 15:29:08 +0530

    Update streamlit_app.py
    Typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ try:
       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
       streamlit.dataframe(fruityvice_normalized)
       
-      expect URLError as e:
+except URLError as e:
         streamliot.error()
 
 streamlit.write('The user entered ', fruit_choice)

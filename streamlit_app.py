58db37384198bea6d2176c05e8b2cc633e65fba3
AbhilashB36 <124914107+AbhilashB36@users.noreply.github.com>
2023-02-11 15:55:57 +0530

    Update streamlit_app.py
    Adding a button

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,7 +55,28 @@ except URLError as e:
 
 streamlit.write('The user entered ', fruit_choice)
 
-streamlit.stop()
+
+
+
+
+#Snowflake-related functions
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("select * from fruit_load_list")
+    return my_cur.fetchall()
+  
+  
+  
+  
+#Add a button to load the fruit
+if streamlit.button('Get Fruit Load list'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)
+  
+  
+  
+  streamlit.stop()
 #The line shown below will tell your py file to use the library you added to the project. 
 import snowflake.connector
 

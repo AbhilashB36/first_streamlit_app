4e2cff660308a5b8a09158f567a9cb96331707d7
AbhilashB36 <124914107+AbhilashB36@users.noreply.github.com>
2023-02-11 16:17:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-streamlit.header('View Our Fruit List - Add Your Favorites!')
+
 #Create Function
 def get_fruityvice_data(this_fruit_choice):
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
@@ -56,7 +56,7 @@ except URLError as e:
 streamlit.write('The user entered ', fruit_choice)
 
 
-
+streamlit.header('View Our Fruit List - Add Your Favorites!')
 #Snowflake-related functions
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:

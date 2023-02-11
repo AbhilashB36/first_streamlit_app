32478f3a0ff4f97ab6388d93ab9c6203e3551171
AbhilashB36 <124914107+AbhilashB36@users.noreply.github.com>
2023-02-11 16:03:09 +0530

    Update streamlit_app.py
    Added a option to Allow the end user to add a fruit to the list

--- streamlit_app.py
+++ streamlit_app.py
@@ -73,7 +73,30 @@ if streamlit.button('Get Fruit Load list'):
   streamlit.dataframe(my_data_rows)
   
   
+
+
+
+
+#Allow the end user to adda a fruit to the list
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("insert into fruit_load_list values('from streamlit')")
+    return "Thanks for adding" + new_fruit
+  
+  
+ add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button("Add a Fruit to the list'):
+                    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+                    back_from_function = insert_row_snowflake(add_my_fruit)
+                    streamlit.text(back_from_function)
+                    
+
+
+
 streamlit.stop()
+
+
+
 #The line shown below will tell your py file to use the library you added to the project. 
 import snowflake.connector
 
